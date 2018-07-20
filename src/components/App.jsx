5c22d63c80d0ee61689ac3c5bea7cea88693ea2f
Andrey Pitunov <andreypitunov@gmail.com>
2018-07-20 11:51:25 +0300

    restyling

--- src/components/App.jsx
+++ src/components/App.jsx
@@ -1,4 +1,5 @@
 import React, { Component } from 'react'
+import styled from 'styled-components'
 
 import logo from 'resources/images/mamon-logo.svg'
 import thumb1 from 'resources/images/thumbs/vadim_karacharski.jpeg'
@@ -10,6 +11,7 @@ import thumb6 from 'resources/images/thumbs/dmitry_shindin.jpg'
 import thumb8 from 'resources/images/thumbs/alex_tukaylo.jpg'
 import thumb9 from 'resources/images/thumbs/artyom_khomyakov.jpeg'
 import thumb10 from 'resources/images/thumbs/agneshka_maksimik.jpg'
+import clients from 'resources/images/clients.png'
 
 const members = [
   {
@@ -95,48 +97,127 @@ const Member = (props) => {
   )
 }
 
+const App__Wrapper = styled.div `
+  max-width: 90rem;
+  height: 100vh;
+  margin: 0 auto;
+`;
+
+const App__Navbar = styled.div `
+  display: flex;
+  justify-content: space-between;
+  align-items: center;
+  width: 100%;
+  height: 7rem;
+  padding: 0 4rem;
+
+  @media screen and (max-width: 520px) {
+    display: none;
+  }
+`;
+
+const App__Logo = styled.div `
+  display: flex;
+
+  ${ props => props._name
+    ? `
+      position: relative;
+      top: 2.5rem;
+      font-weight: 700;
+      font-size: 3.2rem;
+      line-height: 1;
+    `
+    : null
+  }
+`;
+
+const App__Thumb = styled.img `
+  display: block;
+  width: 100%;
+  max-width: 4.8rem;
+  height: auto;
+  margin-right: 2rem;
+`
+
+const App__Contact = styled.div `
+  display: flex;
+  font-size: 2.2rem;
+`;
+
+const App__Btn = styled.a `
+  font-size: 2.2rem;
+  margin-left: 1rem;
+  color: #000;
+  transition: color .2s ease-out;
+  text-decoration: none;
+  cursor: pointer;
+
+  &:hover {
+    color: #5528ff;
+    transition: color .2s ease-out;
+  }
+`;
+
+const App__Container = styled.div `
+  display: flex;
+  flex-direction: column;
+  margin: 4rem;
+`;
+
+const App__Title = styled.div `
+  font-size: 4rem;
+  font-weight: 600;
+  line-height: 5rem;
+  margin-bottom: 6rem;
+
+  @media screen and (max-width: 770px) {
+    font-size: 3rem;
+    line-height: 4rem;
+    margin-bottom: 4rem;
+  }
+`;
+
+const App__Desc = styled.div `
+  font-size: 3rem;
+  font-weight: 300;
+  line-height: 5rem;
+  margin: 2rem 0;
+
+  @media screen and (max-width: 770px) {
+    font-size: 2rem;
+    line-height: 4rem;
+    margin: 1rem 0;
+  }
+`;
+
+const App__Clients = styled.div `
+  width: 100%;
+  height: 5rem;
+  background: url(${clients}) no-repeat;
+  background-size: contain;
+  margin-top: 4rem;
+`;
+
 class App extends Component {
   render() {
     return (
-      <div className="app__wrapper">
-        <div className="app__container">
-          <div className="app__header">
-            <div className="logo">
-              <img className="logo__thumb" src={ logo }></img>
-              <div className="logo__name">mamon</div>
-            </div>
-            <nav className="nav">
-              <ul className="nav__list">
-                {/* <div className="nav__item">
-                  <a href="#" className="nav__link">Services</a>
-                </div>
-                <div className="nav__item">
-                  <a href="#" className="nav__link">Clients</a>
-                </div>
-                <div className="nav__item">
-                  <a href="#" className="nav__link">Our Team</a>
-                </div> */}
-                <div className="nav__item">
-                  <a href="mailto:contact@mamon.pro" className="nav__button">Contact Us</a>
-                </div>
-              </ul>
-            </nav>
-          </div>
-          <p className="app__desc app__desc_main">We're an independent development agency.</p>
-          <p className="app__desc">We understand the intricacies of web development inside-out. That's why we build rock-solid Mobile and Web applications enriched with user-centered visual experiences.</p>
-          <div className="app__team">
-            <div className="app-team">
-              { members.map((member, index) =>
-                <div className="app-team__item" key={ index }>
-                  <Member member={ member } />
-                </div> ) }
-            </div>
-          </div>
-          <div className="section">
-            <div className="section__heading"></div>
-          </div>
-        </div>
-      </div>
+      <App__Wrapper>
+        <App__Navbar>
+          <App__Logo>
+            <App__Thumb src={logo} alt=""/>
+            <App__Logo _name>mamon</App__Logo>
+          </App__Logo>
+          <App__Contact>
+            <div>/</div> <App__Btn href="mailto:contact@mamon.pro">contact@mamon.pro</App__Btn>
+          </App__Contact>
+        </App__Navbar>
+        <App__Container>
+          <App__Title>We're an independent development agency.</App__Title>
+          <App__Desc>We understand the intricacies of web development inside-out. That's why we build rock-solid Mobile and Web applications enriched with user-centered visual experiences.</App__Desc>
+          <App__Desc>We work as one team with our clients in an iterative design and development process.</App__Desc>
+          <App__Clients src={clients}/>
+        </App__Container>
+      </App__Wrapper>
     )
   }
 }
