428961f140422b4882147ccf2a92a8558aa0c9c2
Andrey Pitunov <andreypitunov@gmail.com>
2021-04-26 13:42:13 +0300

    upd

---
+++ src/views/Resume/Resume.jsx
@@ -0,0 +1,20 @@
+import React from 'react'
+
+const Resume = () => {
+  return (
+    <div className='wrapper'>
+      <div className='page'>
+        <div className="container">
+          <div className="resume">
+            <div className="resume__head">
+              <p>Front-end developer</p>
+              <h1>Andrey Pitunov</h1>
+            </div>
+          </div>
+        </div>
+      </div>
+    </div>
+  )
+}
+
+export default Resume
