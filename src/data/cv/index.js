84c5f0b022803309988e94327633e5b3e45ae096
Andrey Pitunov <andreypitunov@gmail.com>
2021-05-04 13:46:44 +0300

    upd

--- src/data/cv/index.js
+++ src/data/cv/index.js
@@ -1,5 +1,7 @@
 import pitunov from './andrey-pitunov-hash.json'
+import makush from './aliaksei-makush-hash.json'
 
 export default {
   'andrey-pitunov-b5896981': pitunov,
+  'aliaksei-makush-t9720172': makush,
 }
